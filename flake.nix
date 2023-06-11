22f8f15e3ad05e7874309de80e340638dbba528d
Joe Sullivan <Nix0S@truckstop.cloud>
2023-06-11 18:57:43 -0500

    added agenix to flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
       # secrets management, lock with git commit at 2023/5/15
-      agenix.url = "github:ryantm/agenix/db5637d10f797bb251b94ef9040b237f4702cde3";
+      agenix.url = "github:ryantm/agenix";
     };
   };
 
@@ -21,8 +21,8 @@
         inherit system;
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
 	  agenix.nixosModules.default
+          home-manager.nixosModules.home-manager
           {
             home-manager = {
               useUserPackages = true;
